a297e655b21ac115af675ad4ee4c46e67735539a
pulkit-s21 <pulkitsaxena795@gmail.com>
2026-07-01 22:59:58 +0530

    Code refactor, debounce timer increased

--- client/src/components/Editor.tsx
+++ client/src/components/Editor.tsx
@@ -1,51 +1,21 @@
-import { useState } from "react"
-import { updateContent, updateTitle } from "../api/document"
-import { socket } from "../socket/socket"
+import {
+  updateDocumentContent,
+  updateDocumentTitle,
+} from "../socket/document.events"
 
 export default function Editor({ doc, setDoc }) {
-  const [oldDoc, setOldDoc] = useState({
-    title: doc.title,
-    content: doc.content,
-  })
-
-  const handleSaveDoc = async (id: string, title: string, content: string) => {
-    if (oldDoc.title !== title) {
-      await updateTitle(id, title)
-    }
-
-    if (oldDoc.content !== content) {
-      await updateContent(id, content)
-    }
-
-    setOldDoc({ title, content })
-  }
-
   const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value
+    setDoc((prev) => (prev ? { ...prev, title: value } : prev))
 
-    setDoc((prev) =>
-      prev
-        ? {
-            ...prev,
-            title: value,
-          }
-        : prev,
-    )
-
-    socket.emit("document:title:update", {
-      documentId: doc.id,
-      title: value,
-    })
+    updateDocumentTitle(doc.id, value)
   }
 
   const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const value = e.target.value
-    setDoc((doc) => ({ ...doc, content: value }))
+    setDoc((prev) => (prev ? { ...prev, content: value } : prev))
 
-    socket.emit("document:update", {
-      documentId: doc.id,
-      content: value,
-    })
+    updateDocumentContent(doc.id, value)
   }
 
   return (
@@ -63,20 +33,6 @@ export default function Editor({ doc, setDoc }) {
           onChange={handleContentChange}
         />
       </div>
-
-      <button
-        disabled={
-          !(oldDoc.title !== doc.title || oldDoc.content !== doc.content)
-        }
-        onClick={() => handleSaveDoc(doc.id, doc.title, doc.content)}
-        style={{
-          width: "fit-content",
-          alignSelf: "end",
-          paddingInline: "1rem",
-        }}
-      >
-        Save
-      </button>
     </>
   )
 }
